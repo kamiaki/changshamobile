4258e2b0499a6aeb9271fd19d295a5b3b089ff13
kamiaki <247557768@qq.com>
2021-04-27 09:59:25 +0800

    配置

--- vue.config.js
+++ vue.config.js
@@ -4,13 +4,18 @@ module.exports = {
   devServer: {
     proxy: {
       '/changsha': {
-        target: 'http://hydftestweb.natapp1.cc',
+        // target: 'http://hydftestweb.natapp1.cc',
+        target: 'http://192.168.80.128:80',
         changeOrigin: true,
         ws: true, // 代理websockets
         pathRewrite: {
           '^/changsha': '/changsha'
         }
       }
+    },
+    disableHostCheck: true,
+    build: {
+      assetsPublicPath: '/mobile'
     }
   }
 }
